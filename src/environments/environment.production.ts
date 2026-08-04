eca14b96a949ea25a972a04ab22a7f9ebf0a3169
Michael Sowah <sowahm@gmail.com>
2026-08-03 19:23:23 -0500

    updated

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,15 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "/api",
+
+  apiBaseUrl: "https://sanctuaryai-backend.onrender.com/api/v1",
+
+  firebase: {
+  apiKey: "AIzaSyALqzi7ibS4kQPX2q5ukosFhSRdem7U2Vc",
+  authDomain: "sanctuaryai-b1012.firebaseapp.com",
+  projectId: "sanctuaryai-b1012",
+  storageBucket: "sanctuaryai-b1012.firebasestorage.app",
+  messagingSenderId: "999508944429",
+  appId: "1:999508944429:web:bd8fcffe78481415dabaa4",
+  measurementId: "G-5JXBCN6RGJ",
+  },
 } as const;
